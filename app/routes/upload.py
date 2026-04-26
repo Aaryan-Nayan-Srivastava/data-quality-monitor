3c4686c58fe8d1e9055b2f2041a8448520cb9e8c
aaryan <anayansrivastava@gmail.com>
2026-04-26 17:36:05 +0530

    Implemented upload route using data_loader service

---
+++ app/routes/upload.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, UploadFile, File, HTTPException,FastAPI
+from app.services.data_loader import save_uploaded_file
+from app.utils.logger import get_logger
+from app.utils.config import DATA_DIR
+
+logger=get_logger(__name__)
+
+router=APIRouter()
+"""
+This endpoint allows users to upload a csv file and save it and returns fild_id(as in job_id) and file path.
+It uses a service fucntion called save_uploaded_file to handle all this.
+"""
+@router.post("/upload")
+def upload_csv(file: UploadFile = File(...)):
+    file_id, file_path = save_uploaded_file(file)
+    logger.info(f"File uploaded and saved: {file_id}")
+    return {
+        "file_id": file_id,
+        "file_name": file.filename,
+        "Status": "Uploaded"
+    }
